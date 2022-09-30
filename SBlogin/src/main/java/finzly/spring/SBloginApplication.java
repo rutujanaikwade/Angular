d0846f5f4cd6bd477890320549029e6929e1b60d
rutujanaikwade <106137290+rutujanaikwade@users.noreply.github.com>
2022-09-30 16:52:11 +0530

    Update SBloginApplication.java

--- SBlogin/src/main/java/finzly/spring/SBloginApplication.java
+++ SBlogin/src/main/java/finzly/spring/SBloginApplication.java
@@ -7,8 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("finzly.spring.controller")
+
 @ComponentScan("finzly.spring.dao.Empdao")
+@ComponentScan("finzly.spring.controller")
 public class SBloginApplication {
 
 	public static void main(String[] args) {
